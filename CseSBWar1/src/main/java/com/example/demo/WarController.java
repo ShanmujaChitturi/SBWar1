7ce3b3528be5b234100938ce798cdc3f253ad130
shanmuja <22MH1A0593@acoe.edu.in>
2025-09-19 09:38:27 +0530

    an war file

---
+++ CseSBWar1/src/main/java/com/example/demo/WarController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WarController {
+
+	@GetMapping("/rocks")
+		public String cserocks() {
+		return "welcome to spring boot war file";
+	}
+	
+}
